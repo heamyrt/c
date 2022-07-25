eb32661f0c901192cf53b85419c4da57d61d4249
heamyrt <95638862+heamyrt@users.noreply.github.com>
2022-07-25 17:14:37 +0800

    Update learning.c

--- learning.c
+++ learning.c
@@ -1,1 +1,20 @@
-
+#include<stdio.h>
+#include<stdlib.h>
+#include<limits.h>
+#include<float.h>
+int getrandvalue(void) {
+	return rand();
+}
+int populate(int* array, int arrsize, int (*getvalue)(void)) {
+	for (int i = 0; i < arrsize;i++){
+		array[i] = getvalue();
+	}
+}
+int main() {
+	int arr[10];
+	populate(arr, 10, getrandvalue);
+	for (int i = 0; i < 5; i++) {
+		printf("%d \n",arr[i]);
+	}
+}
+//返回函数的调用
